5fd3ce2ef54644b84bc770d0484098d0fd6ad39b
mrzo <streetdriver02@gmail.com>
2015-03-01 19:53:56 +0100

    Improve timer waking

--- SmartAlarm/src/sk/jmmobilesoft/smartalarm/service/TimerBroadcastReciever.java
+++ SmartAlarm/src/sk/jmmobilesoft/smartalarm/service/TimerBroadcastReciever.java
@@ -1,6 +1,7 @@
 package sk.jmmobilesoft.smartalarm.service;
 
 import sk.jmmobilesoft.smartalarm.TimerRingActivity;
+import sk.jmmobilesoft.smartalarm.helpers.Helper;
 import sk.jmmobilesoft.smartalarm.log.Logger;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,13 +12,16 @@ public class TimerBroadcastReciever extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Logger.serviceInfo("TimerBroadcastReciever: onRecieve");
+		Helper.wakeLockOn(context);
 		Intent timer = new Intent(context, TimerRingActivity.class);
 		timer.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		timer.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
 		timer.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+		timer.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		timer.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		timer.putExtras(intent);
-		
-		context.startActivity(timer);		
+
+		context.startActivity(timer);
 	}
 
 }
