867e2ac54710dbdc88c8437671d41b2a7043c430
mrzo <streetdriver02@gmail.com>
2015-03-07 00:15:19 +0100

    Autostart fixed

--- SmartAlarm/src/sk/jmmobilesoft/smartalarm/service/ClockRepeatService.java
+++ SmartAlarm/src/sk/jmmobilesoft/smartalarm/service/ClockRepeatService.java
@@ -2,6 +2,9 @@ package sk.jmmobilesoft.smartalarm.service;
 
 import java.util.Calendar;
 
+import sk.jmmobilesoft.smartalarm.helpers.ClockHelper;
+import sk.jmmobilesoft.smartalarm.helpers.GlobalHelper;
+import sk.jmmobilesoft.smartalarm.helpers.Helper;
 import sk.jmmobilesoft.smartalarm.log.Logger;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
@@ -12,14 +15,14 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-public class ClockRepeatService extends Service{
+public class ClockRepeatService extends Service {
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		
+
 		return null;
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Logger.serviceInfo("ClockRepeatService started");
@@ -27,41 +30,30 @@ public class ClockRepeatService extends Service{
 		setRepeatIntent();
 		return START_STICKY;
 	}
-	
+
 	@SuppressLint("NewApi")
-	private void setRepeatIntent(){
+	private void setRepeatIntent() {
 		Intent repeat = new Intent(this, ClockRepeatService.class);
-		PendingIntent pRepeat = PendingIntent.getService(this, 24, repeat, PendingIntent.FLAG_UPDATE_CURRENT);
+		PendingIntent pRepeat = PendingIntent.getService(this, 24, repeat,
+				PendingIntent.FLAG_UPDATE_CURRENT);
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.HOUR_OF_DAY, 23);
 		c.set(Calendar.MINUTE, 59);
 		c.set(Calendar.SECOND, 01);
-		if (c.before(getCurrentTime())) {
-			c.set(Calendar.DATE,
-					Calendar.getInstance().get(Calendar.DATE) + 1);
+		if (c.before(Helper.getCurrentTime())) {
+			c.set(Calendar.DATE, Calendar.getInstance().get(Calendar.DATE) + 1);
 		} else {
-			c.set(Calendar.DATE,
-					Calendar.getInstance().get(Calendar.DATE));
+			c.set(Calendar.DATE, Calendar.getInstance().get(Calendar.DATE));
 		}
-		AlarmManager aManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-		if(android.os.Build.VERSION.SDK_INT < 19){
+		AlarmManager aManager = (AlarmManager) this
+				.getSystemService(Context.ALARM_SERVICE);
+		if (android.os.Build.VERSION.SDK_INT < 19) {
 			aManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pRepeat);
+		} else {
+			aManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(),
+					pRepeat);
 		}
-		else{
-			aManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pRepeat);
-		}
+		ClockHelper.determineAlarmIcon(getApplicationContext());
 		stopSelf();
 	}
-	
-	private Calendar getCurrentTime(){
-		Calendar current = Calendar.getInstance();
-		current.set(Calendar.HOUR_OF_DAY,
-				Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
-		current.set(Calendar.MINUTE, Calendar.getInstance()
-				.get(Calendar.MINUTE));
-		current.set(Calendar.SECOND, Calendar.getInstance()
-				.get(Calendar.SECOND));
-		return current;
-	}
-
 }
